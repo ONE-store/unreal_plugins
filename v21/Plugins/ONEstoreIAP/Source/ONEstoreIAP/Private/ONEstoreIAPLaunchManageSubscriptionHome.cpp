3b4dec2b258440c7e34aa41936fb35562b5d97fc
minu <minu.jang@gmail.com>
2023-01-26 13:47:38 +0900

    Add files via upload

---
+++ v21/Plugins/ONEstoreIAP/Source/ONEstoreIAP/Private/ONEstoreIAPLaunchManageSubscriptionHome.cpp
@@ -0,0 +1,22 @@
+﻿/*
+ * Do Not Modify This File.
+ * Copyright @ 2022, ONEstore. All rights reserved.
+ */
+
+#include "ONEstoreIAPLaunchManageSubscriptionHome.h"
+#include "ONEstoreIAP.h"
+
+UONEstoreIAPLaunchManageSubscriptionHome::UONEstoreIAPLaunchManageSubscriptionHome(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+
+void UONEstoreIAPLaunchManageSubscriptionHome::LaunchManageSubscriptionHome()
+{	
+#if PLATFORM_ANDROID	
+	NativeIapHelper->launchManageSubscription();
+#endif
+}
+
